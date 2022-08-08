4bb15e8d1fee3897e28f0de71245b5ff9247994b
Abdullah AYGÜN <ayguns.abdullah@gmail.com>
2022-08-08 22:17:12 +0300

    Add project files.

---
+++ BackgroundLocationSystem/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackgroundLocationSystem
+{
+    public class StartServiceMessage
+    {
+
+    }
+
+    public class StopServiceMessage
+    {
+
+    }
+
+    public class LocationMessage
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class LocationErrorMessage
+    {
+
+    }
+}
